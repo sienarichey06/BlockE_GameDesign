09094d9c86c902908a0e6d80ec480194c20f426c
Sienarichey06 <richeys24@greenhill.org>
2022-01-21 08:55:35 -0600

    poop

---
+++ Game Design/1st code test average.py
@@ -0,0 +1,30 @@
+from enum import Flag
+import os
+#Siena M Richey
+#Jan 14, 2022
+# Declare variables, print variables, print type of data, learn some operators
+# learn some operators
+
+# this symbol is for comments, means the computer will ignore
+# I want to clear my terminal
+os.system('cls')
+# Program is Average of 3 tests
+
+#Declare and Assign Values
+test1=89
+test2=78.5
+test3=86
+
+#to display things on the screen we use the function print
+# print(type(test1), type(test2), type(Flag)) 
+
+#declare sum to add test symbols for addition is +
+Sum = test1 + test2 + test3 
+print('The Sum of tests is', Sum)
+#average we will use division ... /
+Average= Sum/3 
+print('The Average of tests is', Average)
+
+print("Test1 =", test1, end=" ")
+print("Test2", test2)
+print("The Average of the 3 tests is", Average)
---
+++ Game Design/equation py backaslash.py
@@ -0,0 +1,28 @@
+#Siena Richey
+#01/19/2022
+
+#Other operators +-*/  **exponent  % modulus 
+# format printing    escape character
+#Program: creating an equation, asking user input, and formatting the output 
+import os
+os.system('cls')
+#Declare variable:
+var1=12.3
+var2=1.3
+var3=10
+var4=5
+
+#Calculate equation given
+
+#print(2**4)
+
+print("The Result Is = %6.3f"% var2)
+
+result= (3*var1 - 2*(var2)**2/var3)/var4
+print("var1= %9.2f"% var1)
+print("var2= %9.2f"% var2)
+print("var3= %9.2f"% var3)
+print("var4= %9.2f"% var4)
+print("result = %6.3f"% result, end=" ----")
+
+print("\n The \" quotes\" tabs \t backslash  \\")
