898b9082191548a31843f020a3b2b18a189f5e32
Sienarichey06 <richeys24@greenhill.org>
2022-02-08 09:56:03 -0600

    coding

---
+++ Word Guessing.py
@@ -0,0 +1,63 @@
+#SienaRichey
+#02/08/2022
+#Word Game with 3 levels: 
+#       1. Fruits
+#       2. Animals 
+#       3. COmputer Parts   
+# Choice:
+
+#Create word lists
+from modulefinder import IMPORT_NAME
+import os, random 
+os.system('cls')
+word=""
+guess=""
+def selectWord():
+    global word
+    fruits=["bananas", "grapes", "waterMelon", "papaya", 'oranges', 'tomatoes','mangos', 'kiwis', 
+    'strawberries' 'mangoes', 'blueberries', 'apples']
+
+    # size=(len(fruits))
+    # randy= random.randint(0,size)
+    # print(randy)
+    # word=fruits[randy]
+    # print(word)
+    word=random.choice(fruits)
+def guessFunction():
+    global guess
+    check=True
+    while check:
+        try: 
+            guess=input("\nenter a letter to guess the word ")
+            if guess.isalpha() and len(guess)==1:
+                check=False
+            else:
+                 print("only one letter please")
+        except ValueError:
+            print("only one letter please")
+
+gameOn=True
+tries=0
+letterGuessed=""
+selectWord()
+while gameOn:
+    
+    guessFunction()
+    letterGuessed += guess  #letterGuessed=letterGuessed + guess
+    if guess not in word:
+        tries +=1
+        print(tries)# for testing delete when game is ready
+    countLetter=0
+    for letter in word:
+        if letter in letterGuessed: 
+            print(letter, end=" ")
+            countLetter +=1
+        else: 
+            print("_", end=" ")
+    if tries>6:
+        print("\n Sorry, ran out of chances")
+        #playgame() ask user if they want to play again
+    if countLetter == len(word):
+        print("you guessed it!")
+        #Calculate score
+        #playGame()
