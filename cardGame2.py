807533c0fe9ac600b596993a5bd96f2b6f95d0b1
Sienarichey06 <richeys24@greenhill.org>
2022-02-22 09:55:37 -0600

    commits

--- cardGame2.py
+++ cardGame2.py
@@ -10,7 +10,11 @@ deck=[]
 numberCards = []
 suits = ['h',"d", "c", "s"]
 royals = ["J", "Q", "K", "A"]
+tempplayer1 = []
+tempplayer2 = []
+halfdeck = 0
     
+
 
 #using loops and append to add our content to numberCards :
 def carddecks():
@@ -30,8 +34,6 @@ def carddecks():
     #this will add the card faces to the base list
     #Create full deck
 
-
-
     for k in range(4):   # four suits
         for l in range(13): #13 cards per suit
             card = (numberCards[l] + " " + suits[k])
@@ -39,74 +41,94 @@ def carddecks():
             deck.append(card)
             #this adds the information to the "full deck" we want to make
 #you can print the deck here, if you want to see how it looks
-
-
 #print(deck)
 #now let's see the deck!
-counter=0
 
 def printdecks():
+    global row, col
     counter=0
-
     for row in range(4):
         for col in range(13):
           print(deck[counter], end=" ")
     counter +=1
     print()
+
+def realShuffle():
+    global player1
+    global player2
 #now let's shuffle our deck!    
 #Shuffle the deck cards
-random.shuffle(deck)
-player1=[]
-player2=[]
-# you could print it again here just to see how it shuffle
-#loop to devide the cards to each player
-def splitcards():
-    global numberofcards
+    random.shuffle(deck)
+    player1=[]
+    player2=[]
+    # you could print it again here just to see how it shuffle
+    #loop to devide the cards to each player
+
     for l in range(52):
-        if l%2==0:
-            player1.append(deck[l])
-        else:
-            player2.append(deck[l])
+            if l%2==0:
+                player1.append(deck[l])
+            else:
+                player2.append(deck[l])
+
+   
+def splitdeck():
+    global halfDeck
+    halfDeck = int(len(deck)/2)
 
 
 #print("player1 ",player1)
 #print()
 #print("player2 ",player2)
-halfDeck=int(len(deck)/2)
-plyr1=0
-plyr2=0
+# halfDeck=int(len(deck)/2)
+# plyr1=0
+# plyr2=0
 
     #ask user to hit a key to release cards
-numberofcards=52
-
-def playgame():
-    global click
+halfDeck=int(len(deck)/2)
+GameOn=True
+while GameOn:
     carddecks()
-    splitcards()
+    splitdeck()
+    realShuffle()
     numberofcards=0
-    halfDeck=int(len(deck)/2)
-    plyr1=0
-    plyr2=0
     for i in range (0,halfDeck):
         click=input("Press a any key to get cards")
         print("Player 1     Player 2")
         print("     "+player1[i]+"      "+player2[i])
         if player1[i]>player2[i]:
-            plyr1 +=1
-            numberofcards += 1
+            tempplayer1.append(player1[i])
+            tempplayer1.append(player2[i])
         elif player1[i]<player2[i]:
-            plyr2 +=1
-            numberofcards - 1
-        elif player1[i] == player2[i]:
-            plyr1 +=0 
-            plyr2 +=0
-        print("Player I: "+str(plyr1)+"     Player II: "+ str(plyr2))
-    if plyr1>plyr2:
-        print("Player one won the game "+str(plyr1)+" to "+str(plyr2))
-    else:
-        print("Player two won the game "+str(plyr2)+" to "+str(plyr1))
+            tempplayer2.append(player1[i])
+            tempplayer2.append(player2[i])
+        elif player1 [i] == player2:
+            tempplayer1.append(player1[i])
+            tempplayer2.append(player2[i])
 
-playgame()
+    if (len(tempplayer2)) == 0:
+        print("player 1 won the game!")
+        GameOn= False
+    elif (len(tempplayer1)) == 0:
+        print("player 2 won the game!")
+    else: 
+        print ("in", halfdeck)
+        print("length=", len(player1))
+        for j in range (0, int(halfDeck/2)):
+            player1.pop(j)
+            player2.pop(j)
+        player1.extend(tempplayer1)
+        player2.extend(tempplayer2)
+        if len(player1)<len(player2):
+            halfDeck = len(player2)
+        else:
+            halfDeck = len(player2)
+        
+        
+    #     print("Player I: "+str(plyr1)+"     Player II: "+ str(plyr2))
+    # if plyr1>plyr2:
+    #     print("Player one won the game "+str(plyr1)+" to "+str(plyr2))
+    # else:
+    #     print("Player two won the game "+str(plyr2)+" to "+str(plyr1))
+
 
 #after a round, the user who wins gets all of the cards from the rounds that they won
-
