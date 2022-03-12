3c3c5d4ade46b40dbf30f6d790960dc6c7bd3494
Sienarichey06 <richeys24@greenhill.org>
2022-03-11 18:48:47 -0600

    commit

--- CircleEatsSquare.py
+++ CircleEatsSquare.py
@@ -40,22 +40,54 @@ colors={'white':[255, 255, 255], 'red': [255, 0, 0], 'aqua':[51,153,255], 'orang
 background= colors.get('pink')
 sq_color=colors.get('navy')
 cr_color=colors.get('white')
+
+MAX=10
+jumpCount=MAX
+JUMP=False
+
 while check:
     screen.fill(background)
     for case in pygame.event.get():
         if case.type==pygame.QUIT:
-            check-False
+            check=False
 
     keys= pygame.key.get_pressed()#this returns a list
     if keys[pygame.K_a] and square.x >=move:
         square.x -= move
-    if keys[pygame.K_d] and square.x:
+    if keys[pygame.K_d] and square.x < WIDTH - (wbox):
         square.x += move
-    if keys[pygame.K_w]:
-        square.y -= move
-    if keys[pygame.K_s]:
-        square.y += move
+    #jumping part
+    if not JUMP:
 
+        if keys[pygame.K_w] and square.y >= move:
+            square.y -= move
+        if keys[pygame.K_s] and square.y < HEIGHT - (hbox):
+            square.y += move
+        if keys[pygame.K_SPACE]:
+            JUMP=True
+    else: 
+        if jumpCount>=- MAX:
+            square.y-= jumpCount*abs(jumpCount)/2
+            jumpCount-= 1
+        else:
+            jumpCount=MAX
+            JUMP=False
+
+
+    if keys[pygame.K_LEFT] and xc >=rad + move:
+           xc -= move
+    if keys[pygame.K_RIGHT] and xc < WIDTH - (rad+move):
+           xc += move
+    if keys[pygame.K_UP] and yc >= (rad + move):
+           yc -= move
+    if keys[pygame.K_DOWN] and yc < HEIGHT - (rad + move):
+           yc += move
+
+    checkCollide=square.collidepoint(xc, yc)
+    if checkCollide:
+        square.x=random.randint(wbox, WIDTH)
+        square.y=random.randint(hbox, HEIGHT)
+        rad += move 
     pygame.draw.rect(screen, sq_color, square)
     pygame.draw.circle(screen, cr_color, (xc, yc), rad)
 
